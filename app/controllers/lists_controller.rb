8f22ced1507bf02f4e47989cc8fd2e7e02234be0
blibotics <blibotics@gmail.com>
2013-07-08 23:10:27 -0400

    Migrating database and Making new and create actions

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,18 @@
-class ListsController < ApplicationController
+class ListsController < ActionController::Base
   def index
   end
 
   def new
+    @item = List.new
   end
 
   def create
+    @item = List.new(params[:id])
+      if @item.save
+        redirect_to lists_path
+      else
+        render :new
+      end
   end
 
   def show
